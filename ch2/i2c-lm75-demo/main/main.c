2bbccd7d8ccb3ebb1aeb5b228bc0f5e174f7f5c8
lwf <1107800529@qq.com>
2024-04-14 21:23:56 +0800

    i2c

---
+++ ch2/i2c-lm75-demo/main/main.c
@@ -0,0 +1,62 @@
+/* i2c - Simple example
+
+   Simple I2C example that shows how to initialize I2C
+   as well as reading and writing from and to registers for a sensor connected over I2C.
+
+   The sensor used in this example is a MPU9250 inertial measurement unit.
+
+   For other examples please check:
+   https://github.com/espressif/esp-idf/tree/master/examples
+
+   See README.md file to get detailed usage of this example.
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+#include <stdio.h>
+#include "esp_log.h"
+#include "driver/i2c_master.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#define TEST_I2C_PORT 0
+#define I2C_MASTER_SCL_IO GPIO_NUM_2
+#define I2C_MASTER_SDA_IO GPIO_NUM_1
+
+
+void app_main(void)
+{
+    i2c_master_bus_config_t i2c_mst_config = {
+        .clk_source = I2C_CLK_SRC_DEFAULT,
+        .i2c_port = TEST_I2C_PORT,
+        .scl_io_num = I2C_MASTER_SCL_IO,
+        .sda_io_num = I2C_MASTER_SDA_IO,
+        .glitch_ignore_cnt = 7,
+        .flags.enable_internal_pullup = true,
+    };
+
+    i2c_master_bus_handle_t bus_handle;
+    ESP_ERROR_CHECK(i2c_new_master_bus(&i2c_mst_config, &bus_handle));
+
+    i2c_device_config_t dev_cfg = {
+        .dev_addr_length = I2C_ADDR_BIT_LEN_7,
+        .device_address = 0x48,
+        .scl_speed_hz = 100000,
+    };
+
+    i2c_master_dev_handle_t dev_handle;
+    ESP_ERROR_CHECK(i2c_master_bus_add_device(bus_handle, &dev_cfg, &dev_handle));
+    while(1)
+    {
+        uint8_t buf[] = {0x00};
+        uint8_t buffer[2];
+        ESP_ERROR_CHECK(i2c_master_transmit_receive(dev_handle, buf, sizeof(buf), buffer, 2, -1));
+        printf("rec %x %x %d\n", buffer[0], buffer[1], sizeof(buf));
+        float temperature = buffer[0] + (buffer[1]>>7)*0.5;
+        printf("temperature = %.2f \n", temperature);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+}
